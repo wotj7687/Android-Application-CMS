6623b3bf9a511c4ff073b1183b6566737145e7bd
TorryTheMaltese <torryiscute@gmail.com>
2019-05-07 18:28:39 +0900

    비밀번호 바꿨어요!

--- app/src/main/java/com/example/wotj7/a0128/LoginActivity.java
+++ app/src/main/java/com/example/wotj7/a0128/LoginActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 //수정 2번입니다 이것이 잘 올라가는지 확인할 것입니다. 한번더
 public class LoginActivity extends AppCompatActivity {
 
-    //됐나??
+    //됐나?????
     String password = "Torry The Best";
     EditText editText;
     Button button;
